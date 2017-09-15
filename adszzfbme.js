268a1f49b49a6cb51363048f7afb5d823cf6ea0a
zzfbme <31991548+zzfbme@users.noreply.github.com>
2017-09-15 21:21:45 +0700

    Create adszzfbme.js

---
+++ adszzfbme.js
@@ -0,0 +1,8 @@
+// JavaScript Document //<br />
+ads = new Array(4);<br />
+ads [0] = ‘</p>
+<div class="ads-box" "item"><a href="http://caragratis.net" target="blank"><img src="https://i2.wp.com/caragratis.net/file/img/banner728X90.png?w=584" data-recalc-dims="1"></a></p>
+<p class="ad"><a href="http://caragratis.net" target="blank"><span><font color="#000000" size="1" face="Arial, Helvetica, sans-serif">AdChoices</font></span></a></p>
+</div>
+<p>‘;<br />
+view = Math.floor(Math.random() * 1); document.write(ads[view]);
